80836490001133fba40f3623c91b35e4501e1f54
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-01-04 22:40:31 -0400

    Feat: Creacion de los DbSet en el contexto

--- PracticaGestionInventario/Data/ApplicationDbContext.cs
+++ PracticaGestionInventario/Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PracticaGestionInventario.Models;
 
 namespace PracticaGestionInventario.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Entradas> Entradas { get; set; }
+        public DbSet<EntradasDetalles> EntradasDetalles { get; set; }
+        public DbSet<Productos> Productos { get; set; }
     }
 }
